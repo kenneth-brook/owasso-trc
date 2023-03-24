5cdfd4fbe96291169fcb379a3869f15bf3a77a1f
Kenneth Brook <kenneth.brook@hotmail.com>
2023-03-24 12:09:51 -0400

    volunteers table started

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import volunteers from './volunteers'
+
+export const schemaTypes = [volunteers]
---
+++ schemas/volunteers.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'volunteers',
+  type: 'document',
+  title: 'Volunteers',
+  fields: [
+    {
+      name: 'fname',
+      type: 'string',
+      title: 'First Name',
+    },
+    {
+      name: 'lname',
+      type: 'string',
+      title: 'Last Name',
+    },
+  ],
+}
