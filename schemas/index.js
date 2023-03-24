b597e28c23be151642b386fe1367ea79af20dbf6
Kenneth Brook <kenneth.brook@hotmail.com>
2023-03-24 14:58:59 -0400

    sponsor levels added

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,6 @@ import volunteers from './volunteers'
 import images from './images'
 import group from './group'
 import events from './events'
+import levels from './levels'
 
-export const schemaTypes = [volunteers, images, group, events]
+export const schemaTypes = [volunteers, images, group, events, levels]
---
+++ schemas/levels.js
@@ -0,0 +1,21 @@
+export default {
+  name: 'levels',
+  type: 'document',
+  title: 'Sponsorship Level',
+  fields: [
+    {
+      name: 'sname',
+      type: 'string',
+      title: 'Sponsorship Level Name',
+    },
+    {
+      name: 'event',
+      type: 'reference',
+      to: [
+        {
+          type: 'events',
+        },
+      ],
+    },
+  ],
+}
