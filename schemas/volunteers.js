6873547727848678d673b939c00846b60a8599a5
Kenneth Brook <kenneth.brook@hotmail.com>
2023-03-24 12:52:27 -0400

    most string fields added to volunteers

--- schemas/volunteers.js
+++ schemas/volunteers.js
@@ -13,5 +13,35 @@ export default {
       type: 'string',
       title: 'Last Name',
     },
+    {
+      name: 'street',
+      type: 'string',
+      title: 'Address',
+    },
+    {
+      name: 'city',
+      type: 'string',
+      title: 'City',
+    },
+    {
+      name: 'state',
+      type: 'string',
+      title: 'State',
+    },
+    {
+      name: 'zip',
+      type: 'string',
+      title: 'Zip Code',
+    },
+    {
+      name: 'phone',
+      type: 'string',
+      title: 'Phone Number',
+    },
+    {
+      name: 'email',
+      type: 'string',
+      title: 'Email Address',
+    },
   ],
 }
